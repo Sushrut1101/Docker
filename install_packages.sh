7d5cbd05d47aa9ecf531ce36257ddaa425dbae58
Sushrut1101 <guptasushrut@gmail.com>
2022-03-06 20:18:05 +0530

    Install the base-devel Package
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- install_packages.sh
+++ install_packages.sh
@@ -7,11 +7,12 @@ sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 pacman -Syyu --noconfirm
 
 # Install Basic Packages
-pacman -S --noconfirm \
+pacman -S --needed --noconfirm \
         sudo nano git curl wget rsync aria2 rclone \
         python2 python3 python-pip zip unzip cmake \
         make neofetch speedtest-cli inetutils cpio \
-        jdk8-openjdk lzip dpkg openssl ccache libelf
+        jdk8-openjdk lzip dpkg openssl ccache libelf \
+        base-devel
 
 # Install Some pip packages
 pip install \
