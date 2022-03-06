d323a6f04c25b9baaa84f9cce82352267d90cfce
Sushrut1101 <guptasushrut@gmail.com>
2022-03-06 19:16:13 +0530

    Update install_packages.sh
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- install_packages.sh
+++ install_packages.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# pacman alias
-alias pacman="pacman --noconfirm"
-
 # Uncomment community [multilib] repository
 sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 
 # Update
-pacman -Syyu
+pacman -Syyu --noconfirm
 
 # Install Basic Packages
-pacman -S \
+pacman -S --noconfirm \
         sudo nano git curl wget rsync aria2 rclone \
         python2 python3 python-pip zip unzip cmake \
         make neofetch speedtest-cli inetutils cpio \
