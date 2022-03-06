6c67b130808ded3b963effa4b40009efe186c3e0
Sushrut1101 <guptasushrut@gmail.com>
2022-03-06 19:47:58 +0530

    Add Time Zone Configuration
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- config.sh
+++ config.sh
@@ -6,3 +6,7 @@ export GIT_EMAIL="guptasushrut@gmail.com"
 
 git config --global user.name "${GIT_USERNAME}"
 git config --global user.email "${GIT_EMAIL}"
+
+# TimeZone Configuration
+export TZ="Asia/Kolkata"
+ln -sf /usr/share/zoneinfo/${TZ} /etc/localtime
