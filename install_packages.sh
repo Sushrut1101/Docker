77201cad9d3ceba5e0b6869328735dac1e7f877f
Panchajanya Sarkar <rsk52959@gmail.com>
2022-04-29 16:01:55 +0530

    docker: Install ncurses
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- install_packages.sh
+++ install_packages.sh
@@ -11,8 +11,8 @@ pacman -S --needed --noconfirm \
 	sudo nano git curl wget rsync aria2 rclone \
 	python2 python3 python-pip zip unzip cmake \
 	make neofetch speedtest-cli inetutils cpio \
-	jdk8-openjdk lzip dpkg openssl ccache libelf \
-	base-devel repo openssh lz4 jq go
+	jdk8-openjdk lzip dpkg openssl ccache repo \
+	libelf base-devel openssh lz4 jq go ncurses
 
 # More Packages
 pacman -S --needed --noconfirm \
