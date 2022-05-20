07fc9900b823f19b2276e5d85a6223209cf72926
Akhil Narang <me@akhilnarang.dev>
2022-05-21 00:50:43 +0530

    velvet: Android Build Environment Setup
    * https://github.com/akhilnarang/scripts/blob/master/setup/fedora.sh
    
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- install_packages.sh
+++ install_packages.sh
@@ -8,3 +8,6 @@ dnf install -y \
 	sudo git nano neofetch tmate aria2 rsync rclone \
 	python python2 zip unzip p7zip p7zip-plugins jq \
 	unrar python3-pip tmate make cmake clang glibc
+
+# Android build Environment
+curl -sL https://raw.githubusercontent.com/akhilnarang/scripts/master/setup/fedora.sh | bash
