1a1e0491ec4bf8a2119bd5077b5e624585bf7c08
Sushrut1101 <guptasushrut@gmail.com>
2022-05-21 01:05:31 +0530

    velvet: scripts: Minor Cleanup
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- scripts/fedora-aosp-env-setup.sh
+++ scripts/fedora-aosp-env-setup.sh
@@ -6,48 +6,16 @@
 # environment for Fedora 31 / Rawhide.
 
 # Packages
-sudo dnf install \
-    android-tools \
-    autoconf213 \
-    bison \
-    bzip2 \
-    ccache \
-    curl \
-    flex \
-    gawk \
-    gcc-c++ \
-    git \
-    glibc-devel \
-    glibc-static \
-    libstdc++-static \
-    libX11-devel \
-    make \
-    mesa-libGL-devel \
-    ncurses-devel \
-    openssl \
-    patch \
-    zlib-devel \
-    ncurses-devel.i686 \
-    readline-devel.i686 \
-    zlib-devel.i686 \
-    libX11-devel.i686 \
-    mesa-libGL-devel.i686 \
-    glibc-devel.i686 \
-    libstdc++.i686 \
-    libXrandr.i686 \
-    zip \
-    perl-Digest-SHA \
-    python2 \
-    wget \
-    lzop \
-    openssl-devel \
-    java-1.8.0-openjdk-devel \
-    ImageMagick \
-    ncurses-compat-libs \
-    schedtool \
-    lzip \
-    vboot-utils \
-    vim
+sud dnf install \
+	android-tools autoconf213 bison	bzip2 ccache \
+	curl flex gawk gcc-c++ git glibc-devel glibc-static \
+	libstdc++-static libX11-devel make mesa-libGL-devel \
+	ncurses-devel openssl patch zlib-devel ncurses-devel.i686 \
+	readline-devel.i686 zlib-devel.i686 libX11-devel.i686 \
+	mesa-libGL-devel.i686 glibc-devel.i686 libstdc++.i686 \
+	libXrandr.i686 zip perl-Digest-SHA python2 wget lzop \
+	openssl-devel java-1.8.0-openjdk-devel ImageMagick \
+	ncurses-compat-libs schedtool lzip vboot-utils vim
 
 # The package libncurses5 is not available, so we need to hack our way by symlinking the required library.
 sudo ln -s /usr/lib/libncurses.so.6 /usr/lib/libncurses.so.5
