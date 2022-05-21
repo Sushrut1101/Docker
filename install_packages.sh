613b3cc0d4857796e64b676642ed64f4fffac1d5
Sushrut1101 <guptasushrut@gmail.com>
2022-05-21 13:20:35 +0530

    velvet: Install more Packages
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- install_packages.sh
+++ install_packages.sh
@@ -7,7 +7,10 @@ dnf update -y
 dnf install -y \
 	sudo git nano neofetch tmate aria2 rsync rclone \
 	python python2 zip unzip p7zip p7zip-plugins jq \
-	unrar python3-pip tmate make cmake clang glibc
+	unrar python3-pip tmate make cmake clang glibc \
+	bc ag unace sharutils uudeview arj cabextract \
+	file-roller dtc brotli axel detox cpio lz4 \
+	python3-devel xz-devel
 
 # Android build Environment
 bash /tmp/scripts/fedora-aosp-env-setup.sh
