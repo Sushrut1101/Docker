f6defaea88a30233cf415cb27abdefdef4da0e53
Sushrut1101 <guptasushrut@gmail.com>
2024-06-03 00:49:59 +0530

    fedora: Nuke `zlib-devel.i686`
    Change-Id: I14014ee5fe301e76c6dd3151d9f1fb74e7bd1fdc
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- scripts/android_build_env.sh
+++ scripts/android_build_env.sh
@@ -115,11 +115,11 @@ elif [[ "$(command -v dnf)" != "" ]]; then
         android-tools autoconf213 bison bzip2 ccache curl patch \
         flex gawk gcc-c++ git glibc-devel glibc-static libstdc++-static \
         libX11-devel make mesa-libGL-devel ncurses-devel openssl \
-        zlib-devel ncurses-devel.i686 readline-devel.i686 zlib-devel.i686 \
+        zlib-devel ncurses-devel.i686 readline-devel.i686 vim lzip \
         libX11-devel.i686 mesa-libGL-devel.i686 glibc-devel.i686 \
         libstdc++.i686 libXrandr.i686 zip perl-Digest-SHA python2 wget \
         lzop openssl-devel java-1.8.0-openjdk-devel ImageMagick \
-        schedtool lzip vboot-utils vim
+        schedtool vboot-utils
 
     # The package libncurses5 is not available, so we need to hack our way by symlinking the required library.
     sudo ln -s /usr/lib/libncurses.so.6 /usr/lib/libncurses.so.5
