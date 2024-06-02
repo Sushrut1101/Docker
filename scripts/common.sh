778108619738edd5dddc26ad1e45f23db9f36046
Sushrut1101 <guptasushrut@gmail.com>
2024-06-03 00:57:59 +0530

    docker: Always fetch latest `transfer`
    Change-Id: I6b62f7e1bf3eea331e4972068d2900d3566f2657
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- scripts/common.sh
+++ scripts/common.sh
@@ -2,3 +2,8 @@
 
 # Download apktool
 wget $(curl -sL https://api.github.com/repos/iBotPeaches/Apktool/releases/latest | grep "browser_download_url" | grep ".jar" | cut -d : -f 2,3 | tr -d '"') -O /usr/local/bin/apktool.jar
+
+# Download transfer
+bash -c "$(curl -sL https://git.io/file-transfer)"
+mv transfer /usr/local/bin/transfer
+chmod +x /usr/local/bin/transfer
