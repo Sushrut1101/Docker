31b16191de7325b390a115cb49489cde08c97d5f
Sushrut1101 <guptasushrut@gmail.com>
2025-06-03 11:27:36 +0530

    docker: Install `uv`
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Install uv
+UV_INSTALL_DIR="/usr/local/bin" bash -c "$(curl -LsSf https://astral.sh/uv/install.sh)"
+rm -rf /usr/local/bin/env{,.fish}
+
 # Download apktool
 wget $(curl -sL https://api.github.com/repos/iBotPeaches/Apktool/releases/latest | grep "browser_download_url" | grep ".jar" | cut -d : -f 2,3 | tr -d '"') -O /usr/local/bin/apktool.jar
 
